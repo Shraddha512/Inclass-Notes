a2276c56213804d3f67a05046dedb6550332ff70
sanaik2 <sanaik2@ncsu.edu>
2015-11-14 18:19:27 -0500

    Adding main page

--- inclassnotes2/src/main/java/com/example/shraddhanaik/inclassnotes/MainActivityPage.java
+++ inclassnotes2/src/main/java/com/example/shraddhanaik/inclassnotes/MainActivityPage.java
@@ -1,5 +1,6 @@
 package com.example.shraddhanaik.inclassnotes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivityPage extends AppCompatActivity {
 
@@ -15,5 +17,14 @@ public class MainActivityPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mViewButton = (Button) findViewById(R.id.button);
+        mViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ViewActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
